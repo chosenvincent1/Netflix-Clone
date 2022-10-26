9dda458feff04f7bdc7402f7d641e9e653feb851
mrchosen <chosenvincent@gmail.com>
2022-10-26 08:26:37 -0700

    Added New Features

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -33,32 +33,38 @@ const Home = ()=> {
             <HomeLine />
 
             <section className="second-home-section">
-                <p className="first-text">
-                    Enjoy on your TV.
-                </p>
-                <p className="second-text">
-                    Watch on Smart TVs,
-                    Playstation, Xbox, Chromecast, Apple TV, Blu-ray players,
-                    and more.
-                </p>
+                <div className="text">
+                    <p className="first-text first-section-text">
+                        Enjoy on your TV.
+                    </p>
+                    <p className="second-text second-section-text">
+                        Watch on Smart TVs,
+                        Playstation, Xbox, Chromecast, Apple TV, Blu-ray players,
+                        and more.
+                    </p>
+                </div>
+                <img src="./images/tv.png" alt="tv" className="second-section-img" />
             </section>
             <HomeLine />
 
             <section className="third-home-section">
-                <p className="first-text">
-                    Download your shows to watch offline.
-                </p>
-                <p className="second-text">
-                    Save your favorites easily and always have something to watch.
-                </p>
+                <div className="third-section-text-container">
+                    <p className="first-text first-section-text">
+                        Download your shows to watch offline.
+                    </p>
+                    <p className="second-text second-section-text">
+                        Save your favorites easily and always have something to watch.
+                    </p>
+                </div>
+                <img src="./images/mobile.jpg" alt="mobile" className="second-section-img" />
             </section>
             <HomeLine />
 
             <section className="fourth-home-section">
-                <p className="first-text">
+                <p className="first-text first-section-text">
                     Watch everywhere.
                 </p>
-                <p className="second-text">
+                <p className="second-text second-section-text">
                     Stream unlimited movies and TV shows on your phone, 
                     tablet, laptop, and TV.
                 </p>
@@ -66,21 +72,44 @@ const Home = ()=> {
             <HomeLine />
 
             <section className="fifth-home-section">
-                <p className="first-text">
-                    Create profiles for kids.
-                </p>
-                <p className="second-text">
-                    Send kids on adventures with their favorite characters
-                    in a space made just for them—free with your membership.
-                </p>
+                <div className="fifth-section-text-container">
+                    <p className="first-text first-section-text">
+                        Create profiles for kids.
+                    </p>
+                    <p className="second-text second-section-text">
+                        Send kids on adventures with their favorite characters
+                        in a space made just for them—free with your membership.
+                    </p>
+                </div>
+                <img src="./images/kids.png" alt="kids" className="second-section-img" />
             </section>
             <HomeLine />
 
             <section className="sixth-home-section">
-                <p className="first-text">
+                <p className="first-text first-section-text">
                     Frequently Asked Questions.
                 </p>
                 <div className="frequent-question-container">
+                    <div className="each-question-container">
+                        <div className="each-question">
+                            <p className="question">What is Netflix</p>
+                            <p className="icon">+</p>
+                        </div>
+                        <div className="answer">
+                            <p>
+                                Netflix is a streaming service that offers a wide 
+                                variety of award-winning TV shows, movies, anime, 
+                                documentaries, and more on thousands of internet-connected devices.
+                                <br />
+                                You can watch as much as you want, 
+                                whenever you want without a single commercial – 
+                                all for one low monthly price. There's always 
+                                something new to discover and new TV shows and movies 
+                                are added every week!
+                            </p>
+                        </div>
+                    </div>
+
                     <div className="each-question-container">
                         <div className="each-question">
                             <p className="question">What is Netflix</p>
